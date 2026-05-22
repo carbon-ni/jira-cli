b4fcdd1278f6b50e53225d1609a96c04d4c02d3e
cristianoliveira <contato@cristianoliveira.com.br>
2026-05-22 11:34:25 +0200

    chore: fix lint issues
    Apply golangci-lint v2 fixes and preallocate slices reported by the linter.

--- pkg/adf/jiramarkdown.go
+++ pkg/adf/jiramarkdown.go
@@ -67,18 +67,18 @@ func nodePanelOpenHook(n Connector) string {
 			if k == "panelType" {
 				switch v {
 				case panelTypeInfo:
-					tag.WriteString(fmt.Sprintf("bgColor=%s", bgColorInfo))
+					fmt.Fprintf(&tag, "bgColor=%s", bgColorInfo)
 				case panelTypeNote:
-					tag.WriteString(fmt.Sprintf("bgColor=%s", bgColorNote))
+					fmt.Fprintf(&tag, "bgColor=%s", bgColorNote)
 				case panelTypeError:
-					tag.WriteString(fmt.Sprintf("bgColor=%s", bgColorError))
+					fmt.Fprintf(&tag, "bgColor=%s", bgColorError)
 				case panelTypeSuccess:
-					tag.WriteString(fmt.Sprintf("bgColor=%s", bgColorSuccess))
+					fmt.Fprintf(&tag, "bgColor=%s", bgColorSuccess)
 				case panelTypeWarning:
-					tag.WriteString(fmt.Sprintf("bgColor=%s", bgColorWarning))
+					fmt.Fprintf(&tag, "bgColor=%s", bgColorWarning)
 				}
 			} else {
-				tag.WriteString(fmt.Sprintf("|%s=%s", k, v))
+				fmt.Fprintf(&tag, "|%s=%s", k, v)
 			}
 		}
 	}
